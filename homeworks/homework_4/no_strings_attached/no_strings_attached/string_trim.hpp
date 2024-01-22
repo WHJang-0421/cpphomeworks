5529595615597287b8a2dc616c9588379716edfe
WHJang-0421 <morrranii@gmail.com>
2024-01-22 11:18:58 +0900

    Revert "Implement Split, Trim and Add Tests"

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace no_strings_attached {
-enum class Side { kLeft, kRight, kBoth };
-std::string Trim(const std::string &str, char char_to_trim, Side side);
-std::string Trim(const std::string &str);
-} // namespace no_strings_attached
