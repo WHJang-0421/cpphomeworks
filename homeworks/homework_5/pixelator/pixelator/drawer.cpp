af6c18fdc655514d998d3d8f21deb0d7da6a102d
Woohyuk Jang <morrranii@gmail.com>
2024-01-29 20:37:33 +0900

    Fix failing tests

--- homeworks/homework_5/pixelator/pixelator/drawer.cpp
+++ homeworks/homework_5/pixelator/pixelator/drawer.cpp
@@ -5,7 +5,7 @@ ftxui::Dimensions Drawer::modifyDim(ftxui::Dimensions input_dim) {
   if (input_dim.dimx == ftxui::Dimension::Full().dimx &&
       input_dim.dimy == ftxui::Dimension::Full().dimy)
     return input_dim;
-  return {2 * input_dim.dimx, input_dim.dimx};
+  return {input_dim.dimx, input_dim.dimx};
 }
 Drawer::Drawer(ftxui::Dimensions dimension)
     : screen(ftxui::Screen::Create(modifyDim(dimension))) {}
@@ -16,8 +16,8 @@ Size Drawer::size() const { return {rows(), cols()}; }
 void Drawer::Set(Image image) {
   for (int i = 0; i < image.rows(); ++i) {
     for (int j = 0; j < image.cols(); ++j) {
-      screen.PixelAt(2 * j, i).background_color = image.at(i, j);
-      screen.PixelAt(2 * j + 1, i).background_color = image.at(i, j);
+      screen.PixelAt(2*j, i).background_color = image.at(i, j);
+      screen.PixelAt(2*j+1, i).background_color = image.at(i, j);
     }
   }
 }
